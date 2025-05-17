e78f209fb8d6ae9f0479a95f057bdf014e966f5c
Youness <ouahmaneyouness14@gmail.com>
2025-05-17 09:09:54 -0700

    Initial Maven project with unit test

---
+++ src/test/java/com/youness/jenkinsdemo/MainTest.java
@@ -0,0 +1,12 @@
+package com.youness.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
